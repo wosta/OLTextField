837611ede647cbfe78a697677afabbac678d477b
weiwang <weiwang1202@126.com>
2016-03-18 10:03:49 +0800

    Initial Commit

---
+++ OLUITextField/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OLUITextField
+//
+//  Created by 魏旺 on 16/3/18.
+//  Copyright © 2016年 olive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OLUITextField/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OLUITextField
+//
+//  Created by 魏旺 on 16/3/18.
+//  Copyright © 2016年 olive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
